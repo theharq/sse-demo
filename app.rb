57654b026206da8dbd78877919bb93fc8d7ea87a
Hildebrando Rueda <harq.soft@gmail.com>
2014-10-23 08:05:24 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+set :server, 'thin'
+connections = []
+
+get '/' do
+  halt erb(:login) unless params[:user]
+  erb :chat, locals: { user: params[:user].gsub(/\W/, '') }
+end
+
+get '/stream', :provides => 'text/event-stream' do
+  stream :keep_open do |out|
+    connections << out
+    out.callback { connections.delete(out) }
+  end
+end
+
+post '/' do
+  connections.each { |out| out << "data: #{params[:msg]}\n\n" }
+  204
+end
